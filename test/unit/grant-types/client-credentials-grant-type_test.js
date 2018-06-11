6c48703105413343530160a5cee8c91036a325b8
yinm <yinm.cminor9@gmail.com>
2018-06-11 23:36:53 +0900

    Add should call `model.saveToken()`

--- test/unit/grant-types/client-credentials-grant-type_test.js
+++ test/unit/grant-types/client-credentials-grant-type_test.js
@@ -24,4 +24,31 @@ describe('ClientCredentialsGrantType', () => {
         .catch(should.fail)
     })
   })
+
+  describe('saveToken()', () => {
+    it('should call `model.saveToken()`', () => {
+      const client = {}
+      const user = {}
+      const model = {
+        getUserFromClient: () => {},
+        saveToken: sinon.stub().returns(true)
+      }
+      const handler = new ClientCredentialsGrantType({ accessTokenLifetime: 120, model: model })
+
+      sinon.stub(handler, 'validateScope').returns('foobar')
+      sinon.stub(handler, 'generateAccessToken').returns('foo')
+      sinon.stub(handler, 'getAccessTokenExpiresAt').returns('biz')
+
+      return handler.saveToken(user, client, 'foobar')
+        .then(() => {
+          model.saveToken.callCount.should.equal(1)
+          model.saveToken.firstCall.args.should.have.length(3)
+          model.saveToken.firstCall.args[0].should.eql({ accessToken: 'foo', accessTokenExpiresAt: 'biz', scope: 'foobar' })
+          model.saveToken.firstCall.args[1].should.equal(client)
+          model.saveToken.firstCall.args[2].should.equal(user)
+          model.saveToken.firstCall.thisValue.should.equal(model)
+        })
+        .catch(should.fail)
+    })
+  })
 })
