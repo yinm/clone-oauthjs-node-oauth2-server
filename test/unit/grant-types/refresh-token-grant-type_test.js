7a3f5751380e440fee5d7589bf9d47f747b45c6c
yinm <yinm.cminor9@gmail.com>
2018-06-13 23:05:03 +0900

    Add should call `model.revokeToken()`

--- test/unit/grant-types/refresh-token-grant-type_test.js
+++ test/unit/grant-types/refresh-token-grant-type_test.js
@@ -51,4 +51,24 @@ describe('RefreshTokenGrantType', () => {
     })
   })
 
+  describe('revokeToken()', () => {
+    it('should call `model.revokeToken()`', () => {
+      const model = {
+        getRefreshToken: () => {},
+        revokeToken: sinon.stub().returns({ accessToken: 'foo', client: {}, refreshTokenExpiresAt: new Date(new Date() / 2), user: {} }),
+        saveToken: () => {}
+      }
+      const handler = new RefreshTokenGrantType({ accessTokenLifetime: 120, model: model })
+      const token = {}
+
+      return handler.revokeToken(token)
+        .then(() => {
+          model.revokeToken.callCount.should.equal(1)
+          model.revokeToken.firstCall.args.should.have.length(1)
+          model.revokeToken.firstCall.args[0].should.equal(token)
+          model.revokeToken.firstCall.thisValue.should.equal(model)
+        })
+    })
+  })
+
 })
