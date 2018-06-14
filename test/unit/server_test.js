c1ee809d156c570fdea92432c6ed8db77cbbcafa
yinm <yinm.cminor9@gmail.com>
2018-06-14 21:27:14 +0900

    Add should map string passed as `options` to `options.scope`

--- test/unit/server_test.js
+++ test/unit/server_test.js
@@ -22,5 +22,21 @@ describe('Server', () => {
       AuthenticateHandler.prototype.handle.restore()
     })
 
+    it('should map string passed as `options` to `options.scope`', () => {
+      const model = {
+        getAccessToken() {},
+        verifyScope() {},
+      }
+      const server = new Server({ model: model })
+      sinon.stub(AuthenticateHandler.prototype, 'handle').returns(Promise.resolve())
+      server.authenticate('foo', 'bar', 'test')
+
+      AuthenticateHandler.prototype.handle.callCount.should.equal(1)
+      AuthenticateHandler.prototype.handle.firstCall.args[0].should.equal('foo')
+      AuthenticateHandler.prototype.handle.firstCall.args[1].should.equal('bar')
+      AuthenticateHandler.prototype.handle.firstCall.thisValue.should.have.property('scope', 'test')
+      AuthenticateHandler.prototype.handle.restore()
+    })
+
   })
 })
