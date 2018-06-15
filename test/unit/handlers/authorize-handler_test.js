49a4689151e45be927ed1a5a4da10f9a2f0d94e9
yinm <yinm.cminor9@gmail.com>
2018-06-16 06:01:52 +0900

    Add should call `model.getClient()`

--- test/unit/handlers/authorize-handler_test.js
+++ test/unit/handlers/authorize-handler_test.js
@@ -26,4 +26,25 @@ describe('AuthorizeHandler', () => {
         .catch(should.fail)
     })
   })
+
+  describe('getClient()', () => {
+    it('should call `model.getClient()`', () => {
+      const model = {
+        getAccessToken() {},
+        getClient: sinon.stub().returns({ grants: ['authorization_code'], redirectUris: ['http://example.com/cb'] }),
+        saveAuthorizationCode() {},
+      }
+      const handler = new AuthorizeHandler({ authorizationCodeLifetime: 120, model: model })
+      const request = new Request({ body: { client_id: 12345, client_secret: 'secret' }, headers: {}, method: {}, query: {} })
+
+      return handler.getClient(request)
+        .then(() => {
+          model.getClient.callCount.should.equal(1)
+          model.getClient.firstCall.args.should.have.length(2)
+          model.getClient.firstCall.args[0].should.equal(12345)
+          model.getClient.firstCall.thisValue.should.equal(model)
+        })
+    })
+  })
+
 })
